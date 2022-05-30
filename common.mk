LineageOS/android_device_oneplus_sm8350-common

sm8350-common: Support UDFPS on AoD
Change-Id: I7196dce967c9adbc62bb5156ade749c92c7780a1

common.mk
@@ -387,9 +387,13 @@ PRODUCT_PACKAGES += \
     librmnetctl
 
 # Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
-    libsensorndkbridge
+    libsensorndkbridge \
+    sensors.oplus
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \

crackedpotato007/android_device_oneplus_benz

common.mk
@@ -377,9 +377,13 @@ PRODUCT_PACKAGES += \
     librmnetctl
 
 # Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
-    libsensorndkbridge
+    libsensorndkbridge \
+    sensors.oplus
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \