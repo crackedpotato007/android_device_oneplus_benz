renoir-development/android_device_xiaomi_sm8350-common

sm8350-common: Enforce vendor tree presence
A compiled build has no chance of booting without the prebuilt
binaries, so fail the build if those are not present.

Change-Id: Iadd43e887d426732e549969ab8deb14194ca818d

BoardConfigCommon.mk
@@ -231,4 +231,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit proprietary blobs
--include vendor/xiaomi/sm8350-common/BoardConfigVendor.mk
+include vendor/xiaomi/sm8350-common/BoardConfigVendor.mk

crackedpotato007/android_device_oneplus_benz

Change-Id: I8124a5f92a75490ce48422b9d760764733efe37e

BoardConfigCommon.mk
@@ -241,4 +241,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
--include vendor/oneplus/sm8350-common/BoardConfigVendor.mk
+include vendor/oneplus/sm8350-common/BoardConfigVendor.mk