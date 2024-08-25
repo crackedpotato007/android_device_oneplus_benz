508b8bcd61318504d28906831dde34ba2d0fb270
Michael Bestas <mkbestas@lineageos.org>
2024-08-25 21:46:46 +0300

    benz: Patch libwfdservice to resolve duplicate  dependencies
    Fixes:
    error: module "libwfdservice" variant
    "android_arm_armv8-a_cortex-a76_static": depends on multiple
    versions of the same aidl_interface:
    android.media.audio.common.types-V2-cpp-source,
    android.media.audio.common.types-V4-cpp-source

--- extract-files.py
+++ extract-files.py
@@ -97,6 +97,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libcodec2_shim.so'),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libhidlbase_shim.so'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
